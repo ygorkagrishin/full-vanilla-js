eafd5c20b22227ec9d17b78f10e40dd69bab6e22
ygorkagrishin <webygorkagrishin@gmail.com>
2018-01-11 16:41:21 +0300

    Создана навигационная панель, создан метод обработки события при клике на элемент имеющий атрибут data-.

--- src/full.script.js
+++ src/full.script.js
@@ -8,8 +8,10 @@ var priv = {};
 priv.param = param;
 
 priv.default = {
-    container : priv.param.container !== undefined ? priv.param.container : 'full',
-    section : priv.param.section !== undefined ? priv.param.section : 'section'
+    container : 'full',
+    section :  'section',
+    navigator : priv.param.navigator !== undefined ? priv.param.navigator : false,
+    duration : 700
 }
 
 priv.container = document.getElementById( priv.default.container );
@@ -20,7 +22,11 @@ priv.sections = priv.container.getElementsByClassName( priv.default.section );
 priv.comp = {
     wrapper : 'js-full__wrapper',
     section : 'js-full__section',
-    navigator : 'js-full__list'
+    navigator : {
+        name : 'js-full__nav',
+        item : 'js-full__item',
+        link : 'js-full__link'
+    }
 }
 
 // Массив со значениями для data-anchor.
@@ -64,16 +70,63 @@ priv.setHeight = ( function () {
 
 // Если свойство объекта не undefined и массив, то метод проходит циклом по секциям и устанавливает
 // атрибут data-anchor.
-if ( priv.anchor !== undefined && Array.isArray( priv.anchor.array ) )
+if ( priv.anchor.array !== undefined && Array.isArray( priv.anchor.array ) )
 priv.data = (function () {
   for ( var i = 0; i <= priv.sections.length - 1; i++ )
     if ( !priv.sections[i].hasAttribute( priv.anchor[i] ) )
       priv.sections[i].setAttribute( priv.anchor.name, priv.anchor.array[i] );
 })();
 
+// Метод создает навигационную панель
+if ( priv.default.navigator && priv.anchor.array !== undefined && Array.isArray( priv.anchor.array ) )
+priv.navigator = (function () {
+  
+  var navigator = document.createElement( 'ul' ), item, link;
+  priv.container.appendChild( navigator );
+  navigator.classList.add( priv.comp.navigator.name );
+  
+  for ( var i = 0; i <= priv.anchor.array.length - 1; i++ ) {
+    item = document.createElement( 'li' );
+    navigator.appendChild( item );
+    
+    item.classList.add( priv.comp.navigator.item );
+    link = document.createElement( 'a' );
+    item.appendChild( link );
+    link.classList.add( priv.comp.navigator.link );
+    
+    if ( !link.hasAttribute( priv.anchor.name ) )
+      link.setAttribute( priv.anchor.name, priv.anchor.array[i] );
+  }
+})();
+
+// Метод обрабатывающий событие при клике по элементу с атрибутом data-
+priv.draw = function ( e ) {
+  var target = e.target, wrap, getValue, offset;
+
+  if ( target.hasAttribute( priv.anchor.name ) ) { 
+    if ( target.tagName.toLowerCase() === 'a' ) e.preventDefault();
+    getValue = target.getAttribute( priv.anchor.name );
+      
+    for ( var i = 0; i<= priv.sections.length - 1; i++ ) { 
+      if ( priv.sections[i].hasAttribute( priv.anchor.name ) ) { 
+        if ( priv.sections[i].getAttribute( priv.anchor.name ) === getValue ) {
+          offset = priv.sections[i].offsetTop;
+          wrap = priv.container.getElementsByClassName( priv.comp.wrapper );
+          wrap[0].style.transform = 'translateY(-'+ offset +'px)';
+          wrap[0].classList.add( 'active' );
+        }
+      }
+    }
+  }
+}
+
+// Обработчик события скролл при клике по элементу с атрибутом data-
+if ( Array.isArray( priv.anchor.array ) ) priv.container.addEventListener( 'click', priv.draw );
+
 }
 
 var full = new Full({
     container : 'full',
-    anchor : [ 'hero', 'main', 'footer' ]
+    anchor : [ 'hero', 'main', 'footer' ],
+    navigator : true
 });
